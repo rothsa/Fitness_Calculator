81251e9bb3b5787824b814e82ff574def3f3e0bb
Sally Lehman <rothsa@gmail.com>
2013-08-23 01:52:07 -0400

    Update fitness_calculator.rb

--- fitness_calculator.rb
+++ fitness_calculator.rb
@@ -8,7 +8,7 @@ You only need $3 calipers from EBay.
 Women measure triceps, suprailiac, and thigh.
 Men measure chest, abdominal, and thigh.
 
-Usage: ruby fitness_calculator.rb [gender] [chest|triceps] [suprailiac|abdominal] [thigh] 
+Usage: ruby fitness_calculator.rb [gender] [age] [chest|triceps] [suprailiac|abdominal] [thigh] 
 
 Example: ruby fitness_calculator.rb M 27 8 14 14
 "
